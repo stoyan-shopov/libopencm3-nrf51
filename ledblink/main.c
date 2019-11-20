7f24e5f77d9195c90115f32b144618875c33e975
Stoyan Shopov <stoyan.shopov@gmail.com>
2019-11-20 16:56:28 +0200

    Enhance ledblink example

--- ledblink/main.c
+++ ledblink/main.c
@@ -1,28 +1,37 @@
 /* code copied and tweaked from https://github.com/andenore/NordicSnippets/blob/master/examples/gpio_toggle/main.c */
 #include <libopencm3/nrf51/gpio.h>
 
-#define PIN_ROW1	13
-#define PIN_COL1	4
+enum
+{
+	LED_ROW1	= 13,
+	LED_ROW2	= 14,
+	LED_COL1	= 4,
+	BUTTON_A	= 17,
+};
 
 int main(void)
 {
 
-	gpio_configure(PIN_ROW1, PIN_CNF_DIR_OUTPUT, PIN_CNF_INPUT_DISCONNECT, PIN_CNF_PULL_DISABLED, PIN_CNF_DRIVE_S0S1, PIN_CNF_SENSE_DISABLED);
-	gpio_configure(PIN_COL1, PIN_CNF_DIR_OUTPUT, PIN_CNF_INPUT_DISCONNECT, PIN_CNF_PULL_DISABLED, PIN_CNF_DRIVE_S0S1, PIN_CNF_SENSE_DISABLED);
+	gpio_configure(LED_ROW1, PIN_CNF_DIR_OUTPUT, PIN_CNF_INPUT_DISCONNECT, PIN_CNF_PULL_DISABLED, PIN_CNF_DRIVE_S0S1, PIN_CNF_SENSE_DISABLED);
+	gpio_configure(LED_ROW2, PIN_CNF_DIR_OUTPUT, PIN_CNF_INPUT_DISCONNECT, PIN_CNF_PULL_DISABLED, PIN_CNF_DRIVE_S0S1, PIN_CNF_SENSE_DISABLED);
+	gpio_configure(LED_COL1, PIN_CNF_DIR_OUTPUT, PIN_CNF_INPUT_DISCONNECT, PIN_CNF_PULL_DISABLED, PIN_CNF_DRIVE_S0S1, PIN_CNF_SENSE_DISABLED);
 
-	gpio_clear(PIN_COL1);
+	gpio_configure(BUTTON_A, PIN_CNF_DIR_INPUT, PIN_CNF_INPUT_CONNECT, PIN_CNF_PULL_DISABLED, PIN_CNF_DRIVE_S0S1, PIN_CNF_SENSE_DISABLED);
+
+	gpio_clear(LED_COL1);
 
 	// Toggle GPIO ON/OFF.
 	while (1)
 	{
 		uint32_t volatile tmo;
 
-		gpio_set(PIN_ROW1);
+		gpio_set(LED_ROW1);
 		tmo = 1000000;
 		while (tmo--);
 
-		gpio_clear(PIN_ROW1);
+		gpio_clear(LED_ROW1);
 		tmo = 1000000;
 		while (tmo--);
+		(gpio_read(BUTTON_A) ? gpio_clear : gpio_set)(LED_ROW2);
 	}
 }
